953f23a4c5f1361389ffdb96e04ce0d99b5bd1ee
Georgi Todorov <bosshero31@gmail.com>
2024-05-28 17:00:17 +0300

    Add project files.

---
+++ WebApplication2/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System.Diagnostics;
+using WebApplication2.Models;
+
+namespace WebApplication2.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+    }
+}
---
+++ WebApplication2/Models/Reservation.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApplication2.Models
+{
+    public class Reservation
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int RoomId { get; set; }
+        public DateTime ReservationDateTime { get; set; }
+        public string Description { get; set; }
+        public User User { get; set; }
+        public Room Room { get; set; }
+    }
+}
